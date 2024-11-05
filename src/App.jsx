0ecf6cac0551c0e948e473e6f1de0e1358a1fc52
Mikhail <mike144513@gmail.com>
2024-11-05 21:25:28 +0500

    contentType Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Header from './components/Header.jsx'
-import {ways} from "./data.js";
+import React, { useState } from 'react';
+import {ways, differences} from "./data.js";
 import Motivation from './components/Motivation.jsx'
 import Button from './components/Button/Button.jsx'
 
-function handleClick() {
-    console.log('button clicked')
-}
-
 export default function App() {
+    const [contentType, setContentType] = useState(null)
+
+    // console.log('app component render')
+
+    function handleClick(type) {
+        setContentType(type)
+        // console.log(setContentType)
+    }
 
   return (
     <div>
@@ -15,15 +20,24 @@ export default function App() {
         <main>
             <section>
                 <h3>Learn React</h3>
-
                 <ul>
                     <Motivation {...ways[0]} />
                     <Motivation {...ways[1]} />
                     <Motivation {...ways[2]} />
                 </ul>
-                <Button onClick={handleClick}> Button 1 </Button>
-                <Button onClick={handleClick}> Button 2 </Button>
-                <Button onClick={handleClick}> Button 3 </Button>
+            </ section>
+            <section>
+                <Button onClick={()=>handleClick('way')}> Button 1 </Button>
+                <Button onClick={()=>handleClick('easy')}> Button 2 </Button>
+                <Button onClick={()=>handleClick('program')}> Button 3 </Button>
+
+                {contentType ? (
+                    <p>{differences[contentType]}</p>
+                ) : ( null
+                    // <p>Click the Button</p>
+                )}
+
+                { !contentType ? <p>Click the Button</p> : null }
             </section>
         </main>
     </div>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css'
 
 export default function Button({ children, onClick}) {
     return (
-            <button className="button" onClick={onClick}
+            <button className="button" onClick={onClick} onDoubleClick={()=>console.log('d-clk')}
             // onMouseEnter={handleMouseEnter}
             // onDoubleClick={() => console.log('double click')}
                 >{children} </button>
